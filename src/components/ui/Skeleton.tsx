6f37e68848aeb9ca6ad4abffe1e8e37528449b80
Jemimah Jemutai <jemutaijemimah@gmail.com>
2026-08-31 10:15:22 +0300

    refactor(ui): consolidate Skeleton implementation

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import { cn } from '../../lib/utils';
-
-export const Skeleton: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
-  className,
-  ...props
-}) => {
-  return (
-    <div
-      className={cn('animate-pulse rounded-lg bg-surface-container-high/60', className)}
-      {...props}
-    />
-  );
-};
+export { Skeleton } from './LoadingIndicators';
